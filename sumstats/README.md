531d521461bdee8da28dd60e6cfa14b7790bfd1a
nsabell <nabell@octant.bio>
2024-02-27 20:17:04 -0800

    simplify secondary chap script and update readme

--- sumstats/README.md
+++ sumstats/README.md
@@ -3,43 +3,32 @@
 #### Coefficient Contrasts of DMS5 and DMS8
 
 ```
-Rscript src/sumstats-contrast.R \
-    -i mc4r-dms/sumstats/MC4R_new/dms5-combined.sumstats.tsv \
+Rscript sumstats-contrast.R \
+    -i MC4R_new/dms5-combined.sumstats.tsv \
     -c Forsk_2.5e-05 \
-    -o mc4r-dms/sumstats/MC4R_new/MC4R-DMS5-Gs.tsv
+    -o MC4R-DMS5-Gs.tsv
 
-Rscript src/sumstats-contrast.R \
-    -i mc4r-dms/sumstats/MC4R_new/dms5-combined.sumstats.tsv \
+Rscript sumstats-contrast.R \
+    -i MC4R_new/dms5-combined.sumstats.tsv \
     -c NA \
-    -o mc4r-dms/sumstats/MC4R_new/MC4R-DMS5-Gs-unnormalized.tsv
+    -o MC4R-DMS5-Gs-unnormalized.tsv
 
-Rscript src/sumstats-contrast.R \
-    -i mc4r-dms/sumstats/MC4R_new/dms8-combined.sumstats.tsv \
+Rscript sumstats-contrast.R \
+    -i MC4R_new/dms8-combined.sumstats.tsv \
     -c None_0 \
-    -o mc4r-dms/sumstats/MC4R_new/MC4R-DMS8-Gq.tsv
+    -o MC4R-DMS8-Gq.tsv
 
-Rscript src/sumstats-contrast.R \
-    -i mc4r-dms/sumstats/MC4R_new/dms8-combined.sumstats.tsv \
+Rscript sumstats-contrast.R \
+    -i MC4R_new/dms8-combined.sumstats.tsv \
     -c NA \
-    -o mc4r-dms/sumstats/MC4R_new/MC4R-DMS8-Gq-unnormalized.tsv
+    -o MC4R-DMS8-Gq-unnormalized.tsv
 ```
 
 #### Marginal Contrasts of DMS11
 
-NOTE: `contrast-marginal-chaperone.R` accepts only one concentration per treatment, and requires each treatment have a corresponding `-Ipsen` condition, e.g. `aMSH` and `aMSH-Ipsen`.
-
 ```
-Rscript src/contrast-marginal-chaperone.R \
-    -i mc4r-dms/sumstats/MC4R_new/dms11-combined-marginals.sumstats.tsv \
-    -w aMSH \
-    -c Forsk \
-    -o mc4r-dms/sumstats/MC4R-DMS11-DefectSumstats.tsv \
-    -t aMSH
-
-Rscript src/marginal-contrast.R \
-    -i mc4r-dms/sumstats/MC4R_new/dms11-combined-marginals.sumstats.tsv \
-    -w aMSH \
-    -c Forsk \
-    -o mc4r-dms/sumstats/MC4R-DMS11-RescueSumstats.tsv \
-    -t aMSH
+Rscript contrast-marginal-chaperone.R \
+    -i MC4R_new/dms11-combined-marginals.sumstats.tsv \
+    --defect MC4R-DMS11-DefectSumstats.tsv \
+    --rescue MC4R-DMS11-RescueSumstats.tsv
 ```
--- sumstats/contrast-marginal-chaperone.R
+++ sumstats/contrast-marginal-chaperone.R
@@ -1,72 +1,61 @@
 #!/usr/bin/env Rscript
 # Example usage:
-# Rscript src/contrast-marginal-chaperone.R -i combined.sumstats.tsv -w None_0 -o contrast.tsv -t condition1,condition2
+# Rscript src/contrast-marginal-chaperone.R -i combined.sumstats.tsv -w aMSH -c Forsk -o output.tsv -t aMSH
 
 library(argparse)
 library(tidyverse)
 
 # Command line arguments setup
 parser <- ArgumentParser()
-parser$add_argument("-i", "--input", type = "character", help = "Path to the input summary statistics TSV file", metavar = "input")
-parser$add_argument("-w", "--wt", type = "character", help = "Condition to use as WT reference", metavar = "wt")
-parser$add_argument("-o", "--output", type = "character", help = "Path to the output TSV file", metavar = "output")
-parser$add_argument("-t", "--targets", type = "character", help = "Comma delimited string specifying the conditions to be compared against the control", metavar = "targets")
+parser$add_argument("-i", "--input", type = "character", help = "Path to the input marginals TSV file", metavar = "input", required = TRUE)
+parser$add_argument("-d", "--defect", type = "character", help = "Path to output defect sumstats TSV file", metavar = "defect", required = TRUE)
+parser$add_argument("-r", "--rescue", type = "character", help = "Path to output rescue sumstats TSV file", metavar = "rescue", required = TRUE)
 args <- parser$parse_args()
 
 # Execution
-data <- read_tsv(args$input) %>%
+marginals <- read_tsv(args$input) %>%
     rename("aa" = "mut_aa") %>%
     separate(condition, c("treatment", "concentration"), "_") %>%
     separate(treatment, c("drug", "chaperone"), "-") %>%
     mutate(chaperone = if_else(is.na(chaperone), "NoIpsen", "Ipsen"))
 
-if (!is.null(args$targets)) {
-    target_drugs <- str_split(args$targets, ",")[[1]]
-    data <- data %>%
-        filter(drug %in% c(target_drugs, args$control))
-}
+norm_marginals <- marginals %>%
+    filter(drug != "None") %>%
+    select(-statistic, -p.value, -concentration) %>%
+    pivot_wider(names_from = drug, values_from = log2Marginal:log2MarginalError) %>%
+    mutate(log2MarginalNorm = log2Marginal_aMSH - log2Marginal_Forsk,
+           log2MarginalNormError = sqrt(log2MarginalError_aMSH^2 + log2MarginalError_Forsk^2)) %>%
+    select(chunk, pos, aa, chaperone, log2MarginalNorm, log2MarginalNormError)
 
-wt_reference <- data %>%
-    filter(drug == args$wt, aa == "WT") %>%
-    select(chunk, pos, aa, log2Marginal,log2MarginalError) %>%
-    rename("log2Marginal" = "log2Marginal_WT",
-        "log2MarginalError" = "log2MarginalError_WT")
+wt_noipsen <- norm_marginals %>%
+    filter(chaperone == "NoIpsen", aa == "WT") %>%
+    select(chunk, pos, log2MarginalNorm, log2MarginalNormError) %>%
+    rename("log2MarginalNorm_WT" = "log2MarginalNorm",
+           "log2MarginalNormError_WT" = "log2MarginalNormError")
 
-if (args$control == "NA") {
-    sumstats_contrast <- data %>%
-        left_join(wt_reference,
-            by = c("chunk", "pos", "aa")) %>%
-        mutate(log2ContrastEstimate = log2Marginal - log2Marginal_WT,
-            log2ContrastError = sqrt(log2MarginalError^2 + log2Marginal_WT^2),
-            statistic = log2ContrastEstimate / log2ContrastError,
-            p.value = (1 - pnorm(abs(statistic))) * 2,
-            p.adj = p.adjust(p.value, method = "BH")) %>%
-        select(chunk, pos, aa, log2ContrastEstimate,log2ContrastError,
-            statistic, p.value, p.adj)
-} else {
+defect_sumstats <- norm_marginals %>%
+    filter(aa != "WT") %>%
+    left_join(wt_noipsen) %>%
+    ungroup() %>%
+    mutate(log2FoldChange = log2MarginalNorm - log2MarginalNorm_WT,
+           std.error = sqrt(log2MarginalNormError^2 + log2MarginalNormError_WT^2),
+           statistic = log2FoldChange/std.error,
+           p.value = pnorm(statistic, mean = 0, sd = 1),
+           p.adj = p.adjust(p.value, method = "BH")) %>%
+    select(-log2MarginalNorm:-log2MarginalNormError_WT)
 
-    data_reduced <- data %>% select(chunk, pos, condition, aa, log2Marginal, log2MarginalError)
-    condition_data <- data_reduced %>% filter(drug != args$control)
-    control_data <- data_reduced %>% filter(drug == args$control)
+rescue_sumstats <- norm_marginals %>%
+    filter(aa != "WT") %>%
+    group_by(chunk, pos, aa) %>%
+    pivot_wider(names_from = chaperone,
+                values_from = log2MarginalNorm:log2MarginalNormError) %>%
+    ungroup() %>%
+    mutate(log2FoldChange = log2MarginalNorm_Ipsen - log2MarginalNorm_NoIpsen,
+           std.error = sqrt(log2MarginalNormError_Ipsen^2 + log2MarginalNormError_NoIpsen^2),
+           statistic = log2FoldChange/std.error,
+           p.value = 1-pnorm(statistic, mean = 0, sd = 1),
+           p.adj = p.adjust(p.value, method = "BH")) %>%
+    select(-log2MarginalNorm_Ipsen:-log2MarginalNormError_NoIpsen)
 
-    sumstats_norm <- condition_data %>%
-        inner_join(control_data,
-            by = c("chunk", "pos", "aa", "chaperone"),
-            suffix = c("_cond", "_control")) %>%
-        mutate(log2NormEstimate = log2Marginal_cond - log2Marginal_control,
-            log2NormError = sqrt(log2MarginalError_cond^2 + log2MarginalError_control^2)) %>%
-        select(chunk, pos, aa, log2NormEstimate,log2NormError)
-
-    sumstats_contrast <- sumstats_norm %>%
-        left_join(wt_reference,
-            by = c("chunk", "pos", "aa")) %>%
-        mutate(log2ContrastEstimate = log2Marginal - log2Marginal_WT,
-            log2ContrastError = sqrt(log2MarginalError^2 + log2Marginal_WT^2),
-            statistic = log2ContrastEstimate / log2ContrastError,
-            p.value = (1 - pnorm(abs(statistic))) * 2,
-            p.adj = p.adjust(p.value, method = "BH")) %>%
-        select(chunk, pos, aa, log2ContrastEstimate,log2ContrastError,
-            statistic, p.value, p.adj)
-}
-
-write_tsv(sumstats_contrast, args$output)
+write_tsv(defect_sumstats, args$defect)
+write_tsv(rescue_sumstats, args$rescue)
